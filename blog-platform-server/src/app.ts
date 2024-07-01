e4a3a5914d7c8d26cd84d9e174bcdae4a4b4e9a8
Olena Shchadylo <olena.shchadylo@ukr.net>
2024-07-01 08:04:26 +0300

    server auth

--- blog-platform-server/src/app.ts
+++ blog-platform-server/src/app.ts
@@ -1,12 +1,22 @@
-// @ts-ignore
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 import postsRoutes from './routes/posts';
+import authRoutes from './routes/auth';
+import usersRoutes from './routes/users';
+import { adminAuth, userAuth } from './controllers/auth';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
+app.use('/api/auth', authRoutes);
 app.use('/api', postsRoutes);
+app.use('/api', usersRoutes);
+
+app.get('/admin', adminAuth, (req, res) => res.send('Admin Route'));
+app.get('/basic', userAuth, (req, res) => res.send('User Route'));
 
 app.get('/', (req, res) => res.send('Server running'));
 
